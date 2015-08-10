3155e4189b3902ea187eb6a461fc596977e2ac76
jeffthemaximum <frey.maxim@gmail.com>
2015-08-10 12:05:25 -0400

    swapcase in py

---
+++ Python/Easy/swapCase.py
@@ -0,0 +1,11 @@
+line = "jAVAsCRIPT LANGUAGE 1.8"
+new_line = [c.swapcase() for c in line]
+print ''.join(new_line)
+
+'''
+
+line = [c.lower() for c in line if c == c.upper()]
+line = [c.upper() for c in line if c == c.lower()]
+
+print ''.join(line)
+'''
